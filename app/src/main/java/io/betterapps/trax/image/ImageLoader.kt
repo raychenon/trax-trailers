2e1b01beeb75c9cf13d8c85c688f01f196a1cde0
Raymond Chenon <raymond.chenon@gmail.com>
2019-10-29 16:25:31 +0100

    add placeholder and error image on Image loader

--- app/src/main/java/io/betterapps/trax/image/ImageLoader.kt
+++ app/src/main/java/io/betterapps/trax/image/ImageLoader.kt
@@ -2,12 +2,18 @@ package io.betterapps.trax.image
 
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
+import io.betterapps.trax.R
 
+/**
+ * Facade to any image libraries (Picasso, Fresco, Glide), so it is easier to change to another library.
+ */
 object ImageLoader {
 
     fun load(imageView: ImageView, url: String) {
         Picasso.get()
             .load(url)
+            .placeholder(R.drawable.loading)
+            .error(R.drawable.ic_launcher_round)
             .fit()
             .into(imageView)
     }
