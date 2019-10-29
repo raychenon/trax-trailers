0a0f7eaadf5bfe82fdebdff1b533e94b3026ca2c
Raymond Chenon <raymond.chenon@gmail.com>
2019-10-29 16:16:16 +0100

    use Network security configuration

--- app/src/main/java/io/betterapps/trax/image/ImageLoader.kt
+++ app/src/main/java/io/betterapps/trax/image/ImageLoader.kt
@@ -8,8 +8,7 @@ object ImageLoader {
     fun load(imageView: ImageView, url: String) {
         Picasso.get()
             .load(url)
-            .resize(150, 150)
-            .centerCrop()
+            .fit()
             .into(imageView)
     }
 }
